47c737c29d4d7f769495c97a493f1bc6bedcc7c7
Gordey <gordej.taran@mail.ru>
2025-05-02 21:40:36 +0500

    first_commit_in_develop

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Product(models.Model):
+
+    product_name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+    image = models.ImageField(verbose_name='Изображение')
+    category = models.CharField(max_length=150, verbose_name='Категория')
+    price = models.CharField(max_length=150, verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.product_name} {self.description} {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['product_name',]
+
+
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=150, verbose_name='Название категории')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.category_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['category_name',]
