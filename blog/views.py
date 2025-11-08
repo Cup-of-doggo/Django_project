c14aa7b22f97f7917bea413c2a926fcd29d438c7
Gordey <gordej.taran@mail.ru>
2025-11-08 22:54:02 +0500

    08/11_commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import Blog
 
 
@@ -16,12 +16,23 @@ class BlogListView(ListView):
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
 
+    def get_queryset(self):
+        """Возвращает список опубликованных статей."""
+        queryset = super().get_queryset()
+        return queryset.filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
     template_name = 'blog/blog_detail.html'
     context_object_name = 'blog'
 
+    def get_object(self, queryset=None):
+        """Считает просмотры статей."""
+        counter = super().get_object()
+        counter.views_count += 1
+        counter.save()
+        return counter
 
 
 class BlogUpdateView(UpdateView):
@@ -29,6 +40,10 @@ class BlogUpdateView(UpdateView):
     fields = ['title', 'content', 'preview', 'created_at', 'publication_sign', 'views']
     template_name = 'blog/blog_form.html'
     success_url = reverse_lazy('blog_list')
+
+    def get_success_url(self):
+        """Перенаправление на страницу деталей только что отредактированного объекта"""
+        return reverse('blog_detail', kwargs={'pk': self.object.pk})
 
 
 class BlogDeleteView(DeleteView):
